6d320366f400af18eab159c3e5ebee02fa72acb0
Jake Krueger <jmk4u14@gmail.com>
2021-01-27 20:04:35 -0500

    added local storage function to save button

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+    // Date and Time at top of page
     $("#currentDay").text(moment().format('lll'));
+
+    // Saves info to local storage
+    $(".saveBtn").onClick(function() {
+        var description = $(this).siblings(".description").val();
+        var hour = $(this).parent().attr("id");
+        localStorage.setItem(description, hour);
+    });
+
+    function currentTime() {
+        var 
+    }
+
 });
