69ff21c1306c45d50c7a3d0af81dfa85c05931e1
Jake Krueger <jmk4u14@gmail.com>
2021-01-27 22:29:17 -0500

    Updated buttons to fit row. Gave color attributes for the past, present, and future time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
         localStorage.setItem(timeSlot, description);
     });
 
+    // Load local storage
     $("#nine .description").val(localStorage.getItem("nine"));
     $("#ten .description").val(localStorage.getItem("ten"));
     $("#eleven .description").val(localStorage.getItem("eleven"));
@@ -19,4 +20,27 @@ $(document).ready(function(){
     $("#four .description").val(localStorage.getItem("four"));
     $("#five .description").val(localStorage.getItem("five"));
 
+    // Affects the color of the time block to be past, present, or future
+    function currentTime() {
+        var currentHour = moment().hour();
+        $(".time-block").each(function() {
+            var hour = parseInt($(this).attr("id"));
+
+        if (hour < currentHour) {
+            $(this).addClass("past");
+        } 
+        else if (hour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+
+// Starts the function for the colored time blocks
+currentTime();
 });
