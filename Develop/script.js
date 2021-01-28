c85b5c0aecd93fa956a800ef1af9361175ffdeff
Jake Krueger <jmk4u14@gmail.com>
2021-01-27 22:14:41 -0500

    Finished local storage capabilities

--- Develop/script.js
+++ Develop/script.js
@@ -3,14 +3,20 @@ $(document).ready(function(){
     $("#currentDay").text(moment().format('lll'));
 
     // Saves info to local storage
-    $(".saveBtn").onClick(function() {
+    $(".saveBtn").on("click", function() {
         var description = $(this).siblings(".description").val();
-        var hour = $(this).parent().attr("id");
-        localStorage.setItem(description, hour);
+        var timeSlot = $(this).parent().attr("id");
+        localStorage.setItem(timeSlot, description);
     });
 
-    function currentTime() {
-        var 
-    }
+    $("#nine .description").val(localStorage.getItem("nine"));
+    $("#ten .description").val(localStorage.getItem("ten"));
+    $("#eleven .description").val(localStorage.getItem("eleven"));
+    $("#twelve .description").val(localStorage.getItem("twelve"));
+    $("#one .description").val(localStorage.getItem("one"));
+    $("#two .description").val(localStorage.getItem("two"));
+    $("#three .description").val(localStorage.getItem("three"));
+    $("#four .description").val(localStorage.getItem("four"));
+    $("#five .description").val(localStorage.getItem("five"));
 
 });
